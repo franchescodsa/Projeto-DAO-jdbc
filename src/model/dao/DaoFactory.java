714db3b35d820b1f99feaa9519ab2a6429fca232
franchesco aprigio <franchescodsa@outlook.com>
2024-11-03 20:34:20 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,9 +1,10 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoImpJDBC;
 
 public class DaoFactory {
  public static SellerDao createSellerDao() {
-	 return new SellerDaoImpJDBC();
+	 return new SellerDaoImpJDBC(DB.getConnection());
  }
 }
