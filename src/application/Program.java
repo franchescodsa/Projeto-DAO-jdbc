476d04c7f50a425e785b4c74967ddf642460482a
franchesco aprigio <franchescodsa@outlook.com>
2024-11-03 22:38:53 -0300

    update

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,10 +33,17 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
-		System.out.println("\n= = = = = = TEST 4: seller findByALL = = = =  = =");
+		System.out.println("\n= = = = = = TEST 4: seller insert = = = =  = =");
 		Seller newSeller = new Seller(null, "John Doe", "johndoe@example.com", new Date(), 3000.0, dep);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n= = = = = = TEST 5: seller upadte = = = =  = =");
+		
+		seller= sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completo!");
 		
 		
 		
